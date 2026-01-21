a94dda86c4b150e39f9c2f6f7c20aab3d55af902
Rajdeep Das <rajdeepds626@gmail.com>
2026-01-21 07:43:52 +0000

    fix: prisma client error

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,9 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 
-import { PrismaClient } from "../generated/client";
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
-export * from "../generated/client";
