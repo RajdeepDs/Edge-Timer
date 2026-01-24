7e34f9a5437372af0bee5fa1be073f6fd07a3d44
Rajdeep Das <rajdeepds626@gmail.com>
2026-01-24 15:55:45 +0000

    add timer data table

--- app/components/timer/TimerDataTable.tsx
+++ app/components/timer/TimerDataTable.tsx
@@ -1,4 +1,3 @@
-import { DataTable, Card } from "@shopify/polaris";
 import { StatusBadge } from "../ui/StatusBadge";
 import type { Timer } from "../../types/timer";
 
@@ -8,20 +7,26 @@ interface TimerDataTableProps {
 }
 
 export function TimerDataTable({ timers }: TimerDataTableProps) {
-  const rows = timers.map((timer) => [
-    timer.name,
-    timer.type,
-    <StatusBadge key={timer.id} isPublished={timer.isPublished} />,
-  ]);
-
   return (
-    <Card padding="0">
-      <DataTable
-        columnContentTypes={["text", "text", "text"]}
-        headings={["Timer name", "Type", "Status"]}
-        rows={rows}
-        stickyHeader
-      />
-    </Card>
+    <s-section padding="none">
+      <s-table>
+        <s-table-header-row>
+          <s-table-header listSlot="primary">Timer</s-table-header>
+          <s-table-header listSlot="inline">Type</s-table-header>
+          <s-table-header listSlot="labeled">Status</s-table-header>
+        </s-table-header-row>
+        <s-table-body>
+          {timers.map((timer) => (
+            <s-table-row key={timer.id}>
+              <s-table-cell>{timer.name}</s-table-cell>
+              <s-table-cell>{timer.type}</s-table-cell>
+              <s-table-cell>
+                <StatusBadge isPublished={timer.isPublished} />
+              </s-table-cell>
+            </s-table-row>
+          ))}
+        </s-table-body>
+      </s-table>
+    </s-section>
   );
 }
